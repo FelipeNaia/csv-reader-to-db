92357804685c9c7ef8b20f7943f871d62f308d4e
Felipe Naia <fbruchn@gmail.com>
2021-07-02 04:45:33 -0300

    Incluindo link do video de apresentação

--- README.txt
+++ README.txt
@@ -1,3 +1,4 @@
 AUTOR: Felipe Bruchman Naia
 Código: https://github.com/FelipeNaia/csv-reader-to-db
 Dump: https://drive.google.com/file/d/12sU0hlcWIaYD38MbeUEqGCmF8IRQfGNC/view?usp=sharing
+video: https://drive.google.com/file/d/1Iain_wat85lnCARnrF8gm9oH-dbmTwc5/view?usp=sharing
--- src/main/java/com/br/naia/csvreadertodb/entity/Bem.java
+++ src/main/java/com/br/naia/csvreadertodb/entity/Bem.java
@@ -14,6 +14,7 @@ import javax.persistence.*;
 @Table(name = "BEM", schema = "ELEICAO")
 @SequenceGenerator(name = "SEQ_BEM", sequenceName = "ELEICAO.SEQ_BEM", allocationSize = 1)
 public class Bem {
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO, generator = "SEQ_BEM")
     @Column(name = "BEM_ID")
